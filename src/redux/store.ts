904d5c4f803d545e2f001099ce5e1a43ee7794fe
RaKeD <orlovdanorlov@gmail.com>
2023-10-27 00:45:26 +0300

    Исправление с добавление в store Redux

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,17 +1,22 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./reducers/UserSlice";
 import accountReducer from "./reducers/AccountSlice";
+import categoriesReducer from "./reducers/CategoriesSlice";
+//import brandsReducer from "./reducers/BrandsSlice";
 
 const rootReducer = combineReducers({
   userReducer,
-  account:accountReducer,
+  account: accountReducer,
+  categories: categoriesReducer,
+  //brands: brandsReducer,
 });
 
 export const store = configureStore({
   reducer: {
     rootReducer,
     user: userReducer,
-    account:accountReducer,
+    account: accountReducer,
+    categories: categoriesReducer,
   },
 });
 
