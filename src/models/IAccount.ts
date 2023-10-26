537fe8b9892ecda74d1c638970f75daa62013603
RaKeD <orlovdanorlov@gmail.com>
2023-10-27 00:46:23 +0300

    Удалены лишние параметры из IAccount.ts

--- src/models/IAccount.ts
+++ src/models/IAccount.ts
@@ -1,12 +1,8 @@
 export interface IAccount {
-  // id_account: number;
-  // id_user: number;
-  // login: string;
   email: string;
   password: string;
   id: string;
   roles: IRoles[];
-  // role_id: number;
   // confirmed: boolean;
 }
 export interface IRoles {
