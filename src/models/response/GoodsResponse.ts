9e111b8680a206887a04479e0ce7fe56deaf6165
RaKeD <orlovdanorlov@gmail.com>
2023-10-28 08:43:38 +0300

    Добавлен тип возвращаемых значений для товаров

---
+++ src/models/response/GoodsResponse.ts
@@ -0,0 +1,11 @@
+export interface GoodsResponse {
+  name: string;
+  img?: string;
+  price: number;
+  description?: string;
+  secret: boolean;
+  article?: string;
+  storage: number;
+  brandId?: number;
+  typeId: number;
+}
--- src/services/GoodService.ts
+++ src/services/GoodService.ts
@@ -1,20 +1,22 @@
 import $api from "../http";
+import { GoodsResponse } from "../models/response/GoodsResponse";
+import { AxiosResponse } from "axios";
 export interface CreateGoodDto {
   name: string;
   img?: string;
   price: number | string;
   storage: number | string;
-  description: string;
+  description?: string;
   secret: boolean;
-  type: string;
-  brand: string;
+  typeId: number;
+  brandId: number;
 }
 export default class GoodService {
   static async create(params: CreateGoodDto) {
     console.log("params ", params);
     return $api.post("/goods", { ...params });
   }
-  static async getAll(): Promise<void> {
+  static async getAll(): Promise<AxiosResponse<GoodsResponse>> {
     return $api.get("/goods");
   }
 }
