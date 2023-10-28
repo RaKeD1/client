9a1f920a0e8d6bdebda2ccc9b8d61f3ddca01f37
RaKeD <orlovdanorlov@gmail.com>
2023-10-28 08:44:45 +0300

    Исправлено добавление товаров, добавлен в админ панель

--- src/components/Admin/AdminPanel/index.tsx
+++ src/components/Admin/AdminPanel/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link, Route, Routes, useLocation } from "react-router-dom";
 import Goods from "../../../pages/Admin/Goods";
 import Profile from "../../../pages/Profile";
@@ -8,19 +8,25 @@ import TableTypes from "../../../pages/Admin/Types/TableTypes";
 import AddType from "../../../pages/Admin/Types/AddType";
 import AddBrand from "../../../pages/Admin/Brands/AddBrand";
 import TableBrands from "../../../pages/Admin/Brands";
+import AddGood from "../../../pages/Admin/Goods/AddGood";
+import { useAppSelector } from "../../../redux/hooks/redux";
+import { Alert } from "antd";
 const pages = [
   { id: 1, title: "Аналитика", path: "analytics" },
   { id: 2, title: "Пользователи", path: "profile" },
-  { id: 3, title: "Товары", path: "goods" },
+  { id: 3, title: "Товары", path: "goods/add" },
   { id: 4, title: "Заявки", path: "requests" },
   { id: 5, title: "Категории", path: "types/add" },
   { id: 6, title: "Бренды", path: "brands/add" },
 ];
 const AdminPanel = () => {
   const location = useLocation();
+  const { message, status } = useAppSelector((state) => state.categories);
+  useEffect(() => {}, [message, status]);
   const isActive = (path: any) => location.pathname === "/admin/" + path;
   return (
     <>
+      {message !== "" ? <Alert message={message} type={status} /> : ""}
       <AdminHeader />
       <div className={styles.page}>
         <nav>
@@ -43,7 +49,7 @@ const AdminPanel = () => {
           <Routes>
             <Route path="goods">
               <Route path="" element={<Goods />} />
-              <Route path="add" element={<Goods />} />
+              <Route path="add" element={<AddGood />} />
             </Route>
             <Route path="profile" element={<Profile />} />
             <Route path="categories" />
