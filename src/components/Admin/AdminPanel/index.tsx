9a3e1f8475b860646965a9dd4565160d9b535c46
RaKeD <orlovdanorlov@gmail.com>
2023-11-06 01:14:55 +0300

    Изменено меню для админ панели

--- src/components/Admin/AdminPanel/index.tsx
+++ src/components/Admin/AdminPanel/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, Route, Routes, useLocation } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import Goods from "../../../pages/Admin/Goods";
 import Profile from "../../../pages/Profile";
 import styles from "./adminpanel.module.scss";
@@ -10,38 +10,102 @@ import AddBrand from "../../../pages/Admin/Brands/AddBrand";
 import TableBrands from "../../../pages/Admin/Brands";
 import AddGood from "../../../pages/Admin/Goods/AddGood";
 import AddSliderHome from "../../../pages/Admin/SliderHome/addSlider";
-const pages = [
-  { id: 1, title: "Аналитика", path: "analytics" },
-  { id: 2, title: "Пользователи", path: "profile" },
-  { id: 3, title: "Товары", path: "goods/add" },
-  { id: 4, title: "Заявки", path: "requests" },
-  { id: 5, title: "Категории", path: "types/add" },
-  { id: 6, title: "Бренды", path: "brands/add" },
-  { id: 7, title: "Слайдеры", path: "slider/add" },
+import type { MenuProps } from "antd";
+import { ConfigProvider, Menu } from "antd";
+
+type MenuItem = Required<MenuProps>["items"][number];
+
+function getItem(
+  label: React.ReactNode,
+  key: React.Key,
+  icon?: React.ReactNode,
+  children?: MenuItem[] | null,
+  type?: "group" | null,
+  danger?: boolean,
+): MenuItem {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+    danger,
+  } as MenuItem;
+}
+
+const items: MenuProps["items"] = [
+  getItem("Аналитика", "analytics", null),
+
+  getItem("Пользователи", "sub2", null, [
+    getItem("Все пользователи", "5"),
+    getItem("Админы", "6"),
+  ]),
+
+  getItem("Товары", "sub3", null, [
+    getItem("Список товаров", "goods"),
+    getItem("Добавление товара", "goods/add"),
+  ]),
+
+  getItem("Бренды", "sub4", null, [
+    getItem("Список брендов", "brands"),
+    getItem("Добавление бренда", "brands/add"),
+  ]),
+
+  getItem("Категории", "sub5", null, [
+    getItem("Список категорий", "types"),
+    getItem("Добавление категории", "types/add"),
+  ]),
+
+  getItem("Слайдеры", "sub6", null, [
+    getItem("Список слайдов", "slider"),
+    getItem("Добавление слайда", "slider/add"),
+  ]),
+
+  getItem("Заявки", "sub7", null, [getItem("Список заявок", "requests")]),
+  getItem("Выход", "exit", null),
 ];
+
 const AdminPanel = () => {
-  const location = useLocation();
-  const isActive = (path: any) => location.pathname === "/admin/" + path;
+  const navigate = useNavigate();
+  const onClick: MenuProps["onClick"] = (e) => {
+    if (e.key === "exit") {
+      navigate("/profile");
+    } else {
+      navigate("/admin/" + e.key);
+    }
+    console.log("click ", e);
+    console.log(window.location.pathname);
+  };
   return (
     <>
       <AdminHeader />
-      <div className={styles.page}>
-        <nav>
-          <ul>
-            {pages.map((page) => (
-              <li className={isActive(page.path) ? styles.active : ""}>
-                <Link key={page.id} to={page.path}>
-                  {page.title}
-                </Link>
-              </li>
-            ))}
-            <li>
-              <Link className={styles.exit} to={"/profile"}>
-                Выход
-              </Link>
-            </li>
-          </ul>
-        </nav>
+
+      <div
+        className={styles.page}
+        style={{ display: "flex", margin: "40px 0" }}
+      >
+        <ConfigProvider
+          theme={{
+            components: {
+              Menu: {
+                colorPrimary: "#f0b7a5",
+                itemSelectedBg: "#ffdbd2",
+              },
+            },
+          }}
+        >
+          <Menu
+            onClick={onClick}
+            style={{ width: 350, marginLeft: 20, marginRight: 20 }}
+            defaultSelectedKeys={[
+              window.location.pathname.replace("/admin/", ""),
+            ]}
+            defaultOpenKeys={[window.location.pathname.replace("/admin/", "")]}
+            mode="vertical"
+            items={items}
+          />
+        </ConfigProvider>
+
         <div className={styles.main}>
           <Routes>
             <Route path="goods">
